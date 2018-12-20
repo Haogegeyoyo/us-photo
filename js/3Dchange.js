dabaced72a21fce50f41bd74cd71f5905ebc3384
haogegeyoyo <1468853309@qq.com>
2018-12-20 17:59:16 +0800

    add books

---
+++ js/3Dchange.js
@@ -0,0 +1,137 @@
+$(function(){
+			var $banner=$(".main");
+			var	$picUl=$(".pic ul");
+			var $picLi=$(".pic ul li");
+			var $tabLi=$(".tab li");
+			var $btnA=$(".main .btn a");
+			var $imgWidth=$(".pic ul li img").width();
+			var nowTime = new Date();
+			var index=0;
+
+			var $arr=new Array("p1","p2","p3","p4","p5","p6","p7");
+
+			var timer=setInterval(function(){
+				next();
+			},2000);
+			
+			$btnA.click(function(){
+				// alert(0);
+				i=$(this).index();
+				// i?alert(i):alert(i);判断i？判断是否成立，并弹出i值
+				i?next():prev();
+			})
+			
+			var $M=$(".main");
+			$M.hover(function(){
+					// alert("鼠标移入");
+				clearInterval(timer);
+			},function(){
+				// alert("鼠标移开");
+				timer=setInterval(function(){	
+				next();
+			},2000);
+			})
+
+			function prev(){
+				index--;
+				$arr.push($arr[0]);
+				$arr.shift();
+				$picLi.each(function(i,e){
+					$(e).removeClass().addClass($arr[i]);
+				})
+				show();
+			}
+			function next(){// 图片位置为向左移，即p2-p1,p3-p2，以此，
+				index++;
+				$arr.unshift($arr[6]);
+				$arr.pop();
+				$picLi.each(function(i,e){
+					$(e).removeClass().addClass($arr[i]);
+				})
+				show();
+			}
+			function show(){
+			if(index<0){
+				index=$tabLi.length-1;//下标要减1
+			}
+			else if(index>=$tabLi.length){
+				index=0;
+			}
+			$tabLi.eq(index).addClass('on').siblings().removeClass('on');
+
+			}
+		// 	var timer=setInterval(function(){
+		// 		index++;
+		// 		next();
+		// 	},2000);
+
+		// 	$banner.hover(function(){
+		// 		$(".main .btn").css('display','block');
+		// 		clearInterval(timer);	
+		// 	},function(){
+		// 		$(".main .btn").css('display','none');
+		// 		timer=setInterval(function(){
+		// 		index++;
+		// 		next();
+		// 	},2000);
+		// 	})
+		// 	$tabLi.click(function(){
+		// 		 // alert($(this).index());
+				
+		// 		   index=$(this).index();
+		// 			 $(this).addClass('on').siblings().removeClass('on');
+		// 			 $picUl.animate({//通过改变ul的margin来达到轮播图片的方法，所以这里$picUl.animate
+		// 				marginLeft:-$imgWidth*(index+1)+'px'//index++后，index=1，-$imgWidth*1没有意义，从2开始
+		// 			},300);
+
+		// 		})
+
+		// 	$btnA.click(function(){
+		// 		 if (new Date() - nowTime > 300) {//当前时间减去上一次时间大于300毫秒才执行
+		// 		 	//next()方法动画执行300毫秒，然后后回调函数，回调判断处理index的值，如果不限制时间，
+		// 		 	//那一直点击就会出现，index一直加却没有回调函数，所以处理不了Index,index就会脱离限制范围。
+		// 			nowTime = new Date();
+		// 			i=$(this).index();
+		// 			i?index++:index--;
+		// 			// alert(index);
+		// 			next();	//next方法不是为下一张的方法，当index--时也则为上一张	
+		// 			 // $(".main p").text(index);
+		// 		}	
+		// 	}).mousedown(function () {
+  //       	return false;
+  //   });
+		// 	// function show(){
+		// 	// 	$picUl.animate({
+		// 	// marginLeft:-$imgWidth*(index+1)+'px'
+		// 	// 		},300);
+		// 	// }
+		// 	function next(){
+
+		// 		var indexLi=index;
+		// 		//判断下标，最后一张时按钮也要回到第一个
+		// 		if(indexLi>=$tabLi.length){
+		// 			indexLi=0;
+		// 		}
+		// 		else if(indexLi<0){
+		// 			indexLi=$tabLi.length - 1;
+		// 		}
+
+		// 		$tabLi.eq(indexLi).addClass('on').siblings().removeClass("on");
+
+		// 		$picUl.animate({//通过改变ul的margin来达到轮播图片的方法，所以这里$picUl.animate
+		// 			marginLeft:-$imgWidth*(index+1)+'px'//index++后，index=1，-$imgWidth*1没有意义，从2开始
+		// 		},300,function(){
+		// 		//回调函数，动画函数执行完执行，当第七张图时，让图片立即复位
+		// 			if(index==$tabLi.length){//tabLi是圆点的数，有七个，通过判断这个li的长度
+		// 			//这个方法是上面执行完才会执行，所以会有动画，如果.pic不加头尾的话，就
+		// 			//会有图七到空白的动画，然后再在.pic的li里图七后面加个图一,就会有图七轮播到图七的画面,然后再立即复位到-800px,原始图一处;
+		// 				$picUl.css('marginLeft',-$imgWidth+'px');
+		// 				index=0;
+		// 				// alert($tabLi.length);
+		// 			}else if(index<0){
+		// 				$picUl.css('marginLeft',-$imgWidth*($tabLi.length)+'px');
+		// 				index = $tabLi.length - 1;
+		// 			}
+		// 		});
+		// 	}
+		})
